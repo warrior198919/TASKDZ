317c63a3d167ac226fa895b6ecc667b86f32db4f
Aleksey Graunov <agraunov@gmail.com>
2024-03-13 16:42:38 +0300

    Решение первой Задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,27 +1,20 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-Console.Clear();
+//Console.Clear();
 
 
+using System;
+
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Введите значение натурального числа M:");
-        int m;
-        if (!int.TryParse(Console.ReadLine(), out m) || m < 0)
-        {
-            Console.WriteLine("Ошибка: введите натуральное число для M.");
-            return;
-        }
+        int m = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Введите значение натурального числа N:");
-        int n;
-        if (!int.TryParse(Console.ReadLine(), out n) || n < 0)
-        {
-            Console.WriteLine("Ошибка: введите натуральное число для N.");
-            return;
-        }
+        int n = Convert.ToInt32(Console.ReadLine());
 
         Console.Write($"M = {m}; N = {n} -> \"");
         RangeNaturalNumbers(m, n);
@@ -39,11 +32,11 @@ Console.Clear();
             if (M < N)
             {
                 RangeNaturalNumbers(M, N - 1);
-                if (N >= 0) Console.Write($", {N}");
+                Console.Write($", {N}");
             }
             else if (M > N)
             {
-                if (N >= 0) Console.Write($"{M}, ");
+                Console.Write($"{M}, ");
                 RangeNaturalNumbers(M - 1, N);
             }
         }
