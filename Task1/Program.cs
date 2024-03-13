90ad6b1696d5c101cd8711f61b2361b8de6f41d7
Aleksey Graunov <agraunov@gmail.com>
2024-03-13 14:36:28 +0300

    1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-//Console.Clear();
+Console.Clear();
 
 // Console.WriteLine("Введите значение натурального числа M:");
 // int m = Convert.ToInt32(Console.ReadLine());
@@ -34,40 +34,87 @@
 //     }
 // }
 
-using System;
+// using System;
 
-class Program
+// class Program
+// {
+//     static int Ackermann(int m, int n)
+//     {
+//         if (m == 0)
+//             return n + 1;
+//         else if (n == 0)
+//             return Ackermann(m - 1, 1);
+//         else
+//             return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+
+//     static void Main()
+//     {
+//         Console.Write("Введите значение m: ");
+//         int m;
+//         if (!int.TryParse(Console.ReadLine(), out m))
+//         {
+//             Console.WriteLine("Ошибка: введите целое число для m.");
+//             return;
+//         }
+
+//         Console.Write("Введите значение n: ");
+//         int n;
+//         if (!int.TryParse(Console.ReadLine(), out n))
+//         {
+//             Console.WriteLine("Ошибка: введите целое число для n.");
+//             return;
+//         }
+
+//         int result = Ackermann(m, n);
+//         Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+//     }
+// }
+// using System;
+
+// class Program
 {
-    static int Ackermann(int m, int n)
-    {
-        if (m == 0)
-            return n + 1;
-        else if (n == 0)
-            return Ackermann(m - 1, 1);
-        else
-            return Ackermann(m - 1, Ackermann(m, n - 1));
-    }
-
     static void Main()
     {
-        Console.Write("Введите значение m: ");
+        Console.WriteLine("Введите значение натурального числа M:");
         int m;
-        if (!int.TryParse(Console.ReadLine(), out m))
+        if (!int.TryParse(Console.ReadLine(), out m) || m < 0)
         {
-            Console.WriteLine("Ошибка: введите целое число для m.");
+            Console.WriteLine("Ошибка: введите натуральное число для M.");
             return;
         }
 
-        Console.Write("Введите значение n: ");
+        Console.WriteLine("Введите значение натурального числа N:");
         int n;
-        if (!int.TryParse(Console.ReadLine(), out n))
+        if (!int.TryParse(Console.ReadLine(), out n) || n < 0)
         {
-            Console.WriteLine("Ошибка: введите целое число для n.");
+            Console.WriteLine("Ошибка: введите натуральное число для N.");
             return;
         }
 
-        int result = Ackermann(m, n);
-        Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+        Console.Write($"M = {m}; N = {n} -> \"");
+        RangeNaturalNumbers(m, n);
+        Console.WriteLine("\"");
+    }
+
+    static void RangeNaturalNumbers(int M, int N)
+    {
+        if (M == N)
+        {
+            if (N >= 0) Console.Write($"{N}");
+        }
+        else
+        {
+            if (M < N)
+            {
+                RangeNaturalNumbers(M, N - 1);
+                if (N >= 0) Console.Write($", {N}");
+            }
+            else if (M > N)
+            {
+                if (N >= 0) Console.Write($"{M}, ");
+                RangeNaturalNumbers(M - 1, N);
+            }
+        }
     }
 }
-
