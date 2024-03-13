19770f407cf2791a7e139e5da5a6fefd3414773b
Aleksey Graunov <agraunov@gmail.com>
2024-03-13 15:09:15 +0300

    new string

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,78 +3,9 @@
 
 Console.Clear();
 
-// Console.WriteLine("Введите значение натурального числа M:");
-// int m = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("Введите значение натурального числа N:");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write($"M = {m}; N = {n} -> \"");
-// RangeNaturalNumbers(m, n);
-// Console.WriteLine("\"");
-
-// void RangeNaturalNumbers(int M, int N)
-// {
-//     if (M == N)
-//     {
-//         if (N >= 0) Console.Write($"{N}");
-//     }
-//     else
-//     {
-//         if (M < N)
-//         {
-//             RangeNaturalNumbers(M, N - 1);
-//             if (N >= 0) Console.Write($", {N}");
-//         }
-//         else if (M > N)
-//         {
-//             if (N >= 0) Console.Write($"{M}, ");
-//             RangeNaturalNumbers(M - 1, N);
-//         }
-//     }
-// }
-
-// using System;
-
-// class Program
-// {
-//     static int Ackermann(int m, int n)
-//     {
-//         if (m == 0)
-//             return n + 1;
-//         else if (n == 0)
-//             return Ackermann(m - 1, 1);
-//         else
-//             return Ackermann(m - 1, Ackermann(m, n - 1));
-//     }
-
-//     static void Main()
-//     {
-//         Console.Write("Введите значение m: ");
-//         int m;
-//         if (!int.TryParse(Console.ReadLine(), out m))
-//         {
-//             Console.WriteLine("Ошибка: введите целое число для m.");
-//             return;
-//         }
-
-//         Console.Write("Введите значение n: ");
-//         int n;
-//         if (!int.TryParse(Console.ReadLine(), out n))
-//         {
-//             Console.WriteLine("Ошибка: введите целое число для n.");
-//             return;
-//         }
-
-//         int result = Ackermann(m, n);
-//         Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
-//     }
-// }
-// using System;
-
-// class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Console.WriteLine("Введите значение натурального числа M:");
         int m;
