eedf188946fdfd0cfec8a8af8262f1e433bba8a5
root <root@cograndl.ru>
2024-03-13 19:48:38 +0300

    repair taskdz

--- Task3/obj/Debug/net7.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net7.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cb9b13356e4735d044565dc6ee09781073f20c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a6dac9a1adbd89c362f337c36743742963fa75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
