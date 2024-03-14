ce703f937ce2a2844ef4e67b4a05584619e4072b
root <root@cograndl.ru>
2024-03-14 03:58:17 +0300

    an example of solving a problem. recursion

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello World");
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 4, 3, 2, 1 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.WriteLine(arr[index]);
+        PrintArrayReverse(arr, index - 1);
+    }
+}
--- Task3/obj/Debug/net7.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net7.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a6dac9a1adbd89c362f337c36743742963fa75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eedf188946fdfd0cfec8a8af8262f1e433bba8a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
