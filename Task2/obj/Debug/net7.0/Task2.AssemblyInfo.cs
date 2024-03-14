fb4d63cdb6258a2cd49cc343da7f73de1930ff88
root <root@cograndl.ru>
2024-03-14 04:10:29 +0300

    Решение по заданию N2

--- Task2/obj/Debug/net7.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net7.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a6dac9a1adbd89c362f337c36743742963fa75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c1b138522f0b93d3a75324d354e3596f90ca697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
