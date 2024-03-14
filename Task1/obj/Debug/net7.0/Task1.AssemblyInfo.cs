daa241319453ebf6e58dc603d656365f2b768dc2
root <root@cograndl.ru>
2024-03-14 04:13:06 +0300

    Решение по заданию N1

--- Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a6dac9a1adbd89c362f337c36743742963fa75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4d63cdb6258a2cd49cc343da7f73de1930ff88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
