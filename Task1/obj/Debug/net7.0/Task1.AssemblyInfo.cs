d8459bb9e2c58a735bf2d11fb1c4f9bf9b026476
root <root@cograndl.ru>
2024-03-15 15:11:26 +0300

    add obj task1

--- Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4d63cdb6258a2cd49cc343da7f73de1930ff88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa241319453ebf6e58dc603d656365f2b768dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
